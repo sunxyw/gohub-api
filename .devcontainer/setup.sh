ec0baba6857ab719f8cc9436db56b3fce5d4e06e
sunxyw <31698606+sunxyw@users.noreply.github.com>
2022-06-22 23:03:12 +0800

    add oh-my-zsh setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,12 @@
 # install curl, git, ...
 apt-get update
-apt-get install -y curl git jq
+apt-get install -y curl git jq zsh
 
 useradd -m user
 su user
 
 # install go
-VERSION='1.14'
+VERSION='1.17'
 OS='linux'
 ARCH='amd64'
 
@@ -38,3 +38,10 @@ go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
 go get -u -v github.com/zmb3/gogetdoc
 go get -u -v golang.org/x/lint/golint
 go get -u -v golang.org/x/tools/cmd/gorename
+
+# setup and install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
