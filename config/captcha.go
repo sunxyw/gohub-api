fa29701b0bbf4c1d81ae4f0a0b2bcc1d9828e0aa
sunxyw <31698606+sunxyw@users.noreply.github.com>
2022-06-25 09:21:05 +0000

    add image captcha api

--- config/captcha.go
+++ config/captcha.go
@@ -1,66 +1,34 @@
-// Package captcha 处理图片验证码逻辑
-package captcha
+package config
 
-import (
-	"gohub/pkg/app"
-	"gohub/pkg/config"
-	"gohub/pkg/redis"
-	"sync"
+import "gohub/pkg/config"
 
-	"github.com/mojocn/base64Captcha"
-)
+func init() {
+	config.Add("captcha", func() map[string]interface{} {
+		return map[string]interface{}{
 
-type Captcha struct {
-	Base64Captcha *base64Captcha.Captcha
-}
+			// 验证码图片高度
+			"height": 80,
 
-// once 确保 internalCaptcha 对象只初始化一次
-var once sync.Once
+			// 验证码图片宽度
+			"width": 240,
 
-// internalCaptcha 内部使用的 Captcha 对象
-var internalCaptcha *Captcha
+			// 验证码的长度
+			"length": 6,
 
-// NewCaptcha 单例模式获取
-func NewCaptcha() *Captcha {
-	once.Do(func() {
-		// 初始化 Captcha 对象
-		internalCaptcha = &Captcha{}
+			// 数字的最大倾斜角度
+			"maxskew": 0.7,
 
-		// 使用全局 Redis 对象，并配置存储 Key 的前缀
-		store := RedisStore{
-			RedisClient: redis.Redis,
-			KeyPrefix:   config.GetString("app.name") + ":captcha:",
+			// 图片背景里的混淆点数量
+			"dotcount": 80,
+
+			// 过期时间，单位是分钟
+			"expire_time": 15,
+
+			// debug 模式下的过期时间，方便本地开发调试
+			"debug_expire_time": 10080,
+
+			// 非 production 环境，使用此 key 可跳过验证，方便测试
+			"testing_key": "captcha_skip_test",
 		}
-
-		// 配置 base64Captcha 驱动信息
-		driver := base64Captcha.NewDriverDigit(
-			config.GetInt("captcha.height"),      // 宽
-			config.GetInt("captcha.width"),       // 高
-			config.GetInt("captcha.length"),      // 长度
-			config.GetFloat64("captcha.maxskew"), // 数字的最大倾斜角度
-			config.GetInt("captcha.dotcount"),    // 图片背景里的混淆点数量
-		)
-
-		// 实例化 base64Captcha 并赋值给内部使用的 internalCaptcha 对象
-		internalCaptcha.Base64Captcha = base64Captcha.NewCaptcha(driver, &store)
 	})
-
-	return internalCaptcha
-}
-
-// GenerateCaptcha 生成图片验证码
-func (c *Captcha) GenerateCaptcha() (id string, b64s string, err error) {
-	return c.Base64Captcha.Generate()
-}
-
-// VerifyCaptcha 验证验证码是否正确
-func (c *Captcha) VerifyCaptcha(id string, answer string) (match bool) {
-
-	// 方便本地和 API 自动测试
-	if !app.IsProduction() && id == config.GetString("captcha.testing_key") {
-		return true
-	}
-	// 第三个参数是验证后是否删除，我们选择 false
-	// 这样方便用户多次提交，防止表单提交错误需要多次输入图片验证码
-	return c.Base64Captcha.Verify(id, answer, false)
 }
