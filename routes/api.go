fcdb138fde40b6f4838670c14077dc56fa43ecdd
sunxyw <31698606+sunxyw@users.noreply.github.com>
2022-06-27 13:41:07 +0000

    add update phone api

--- routes/api.go
+++ routes/api.go
@@ -59,6 +59,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 				usersGroup.GET("", uc.Index)
 				usersGroup.PUT("", middlewares.AuthJWT(), uc.UpdateProfile)
 				usersGroup.PATCH("/email", middlewares.AuthJWT(), uc.UpdateEmail)
+				usersGroup.PATCH("/phone", middlewares.AuthJWT(), uc.UpdatePhone)
 			}
 		}
 
