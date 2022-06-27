869823505a7d74fa71bd570c1a4851cc758970d6
sunxyw <31698606+sunxyw@users.noreply.github.com>
2022-06-27 13:36:17 +0000

    add update profile api

--- database/migrations/2022_06_27_151746_add_users_table.go
+++ database/migrations/2022_06_27_151746_add_users_table.go
@@ -1,33 +1,37 @@
 package migrations
 
 import (
-    "database/sql"
-    "gohub/app/models"
-    "gohub/pkg/migrate"
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
 
-    "gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 func init() {
 
-    type User struct {
-        models.BaseModel
+	type User struct {
+		models.BaseModel
 
-        Name     string `gorm:"type:varchar(255);not null;index"`
-        Email    string `gorm:"type:varchar(255);index;default:null"`
-        Phone    string `gorm:"type:varchar(20);index;default:null"`
-        Password string `gorm:"type:varchar(255)"`
+		Name     string `gorm:"type:varchar(255);not null;index"`
+		Email    string `gorm:"type:varchar(255);index;default:null"`
+		Phone    string `gorm:"type:varchar(20);index;default:null"`
+		Password string `gorm:"type:varchar(255)"`
 
-        models.CommonTimestampsField
-    }
+		City         string `gorm:"type:varchar(10);"`
+		Introduction string `gorm:"type:varchar(255);"`
+		Avatar       string `gorm:"type:varchar(255);default:null"`
 
-    up := func(migrator gorm.Migrator, DB *sql.DB) {
-        migrator.AutoMigrate(&User{})
-    }
+		models.CommonTimestampsField
+	}
 
-    down := func(migrator gorm.Migrator, DB *sql.DB) {
-        migrator.DropTable(&User{})
-    }
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&User{})
+	}
 
-    migrate.Add("2022_06_27_151746_add_users_table", up, down)
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&User{})
+	}
+
+	migrate.Add("2022_06_27_151746_add_users_table", up, down)
 }
